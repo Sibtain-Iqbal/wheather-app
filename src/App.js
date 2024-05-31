ff3119839a85d11bccc04e79751f0e23c455fda6
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-05-31 11:09:12 -0700

    Complete whether app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  const [whther ,setwhether] = useState('')
+  const [citydetail ,setdetail] = useState()
+
+
+  const handles =(e)=>{
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${whther}&appid=751d66e130befad396405dc13796a57c&units=metrics`)
+    .then((response)=> response.json())
+    .then((finaldata)=>{
+      if(finaldata.cod =="404"){
+        setdetail(undefined)
+      }
+      else{
+        setdetail(finaldata)
+      }
+    })
+
+    e.preventDefault()
+    console.log(whther);
+    setwhether('')
+  }
+
   return (
     <div className="App">
 
       <div className="head">
         <h1 className="capital">Simple Wheather APP</h1>
 
-        <form>
-          <input
+        <form onSubmit={handles}>
+          <input  
             type="text"
             placeHolder="City Name"
+            value={whther}
+          onChange={(e)=> setwhether(e.target.value)}
           />
           <button>Submit</button>
         </form>
       </div>
 
       <div className="whether">
-        <h1 className="cap-1">Jairpur <span>IN</span> </h1>
-        <h2 className="temp">9.60oc</h2>
+
+        {
+          citydetail !== undefined
+          ?
+          <>
+          <h1 className="cap-1">{citydetail.name} <span>{citydetail.sys.country}</span> </h1>
+        <h2 className="temp">{citydetail.main.temp}</h2>
         <img 
-        src="http://openweathermap.org/img/w/50d.png"
+        src={`http://openweathermap.org/img/w/${citydetail.weather[0].icon}.png`}
         />
-        <p>FOg</p>
+        <p>{citydetail.weather[0].description}</p>
+          </>
+          :
+          "No data found"
+
+        }
+
+
+
+
+
+        
       
       </div>
 
